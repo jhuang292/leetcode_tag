d83cefb5a4825904768ed9e7b1f7a410456ec10a
Junxiong Huang <jhuang292@wisc.edu>
2019-04-22 17:53:35 -0500

    managers with at least 5 direct reports

---
+++ database/570.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT Name
+FROM Employee
+WHERE Id IN (
+                SELECT ManagerId
+                FROM Employee 
+                GROUP BY ManagerId
+                HAVING COUNT(DISTINCT Id) >= 5
+                )
