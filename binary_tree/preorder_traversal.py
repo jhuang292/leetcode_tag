f419ec6ee64ef4bdbc7df45ed8a89243df3fc4dd
Junxiong Huang <jhuang292@wisc.edu>
2019-04-24 17:57:29 -0500

    preorder traversal is to iterate the root first, then left child and right child

---
+++ binary_tree/preorder_traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        
+        res = []
+        self.dfs(root, res)
+        return res
+    
+    def dfs(self, root, res):
+        if root:
+            res.append(root.val)
+            self.dfs(root.left, res)
+            self.dfs(root.right, res)
+            
