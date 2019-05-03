9db2ff8283148a4033de23ccd9b7db8be939bf59
Junxiong Huang <jhuang292@wisc.edu>
2019-05-03 16:19:01 -0500

    length 0f last word

---
+++ math/58.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count, local_count = 0, 0
+        for i in range(len(s)):
+            if s[i] == ' ':
+                local_count = 0
+            else:
+                local_count += 1
+                count = local_count
+        return count
